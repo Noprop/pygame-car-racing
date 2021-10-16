4e8d9bbff860789dc0d743bfb78eb901f3fb2cc9
Greg Forster <12greg21@gmail.com>
2021-10-16 14:07:47 -0400

    car can turn left and right using arrow keys

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import time
 import math
-from utils import scale_image
+from utils import scale_image, blit_rotate_center
 
 GRASS = scale_image(pygame.image.load('assets/grass.jpg'), 2.5)
 TRACK = scale_image(pygame.image.load('assets/track.png'), 0.9)
@@ -19,20 +19,48 @@ pygame.display.set_caption('Racing Game!')
 FPS = 60
 
 
-def draw(win, images_to_draw):
+class AbstractCar:
+    def __init__(self, max_vel, rotation_vel):
+        self.vel = 0
+        self.angle = 0
+        self.img = self.IMG
+        self.max_vel = max_vel
+        self.rotation_vel = rotation_vel
+        self.x, self.y = self.START_POS
+
+    def rotate(self, left=False, right=False):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+
+    def draw(self, win):
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+
+
+class PlayerCar(AbstractCar):
+    IMG = RED_CAR
+    START_POS = (180, 200)
+
+
+def draw(win, images_to_draw, car_to_draw):
     for img, pos in images_to_draw:
         win.blit(img, pos)
+
+    player_car.draw(win)
+    pygame.display.update()
 
 
 run = True
 clock = pygame.time.Clock()
-images = [(GRASS, (0, 0)), (TRACK, (0, 0)), (FINISH, (0, 0)), (RED_CAR, (0, 0)), (GREEN_CAR, (0, 0))]
+print(RED_CAR.get_rect().center)
+images = [(GRASS, (0, 0)), (TRACK, (0, 0)), (FINISH, (0, 0))]
+player_car = PlayerCar(4, 4)
 
 while run:
     clock.tick(FPS)
 
-    draw(WIN, images)
-    pygame.display.update()
+    draw(WIN, images, player_car)
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
@@ -42,5 +70,13 @@ while run:
         if event.type == pygame.QUIT:
             run = False
             break
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        player_car.rotate(left=True)
+    if keys[pygame.K_RIGHT]:
+        player_car.rotate(right=True)
+
 
 pygame.quit()
